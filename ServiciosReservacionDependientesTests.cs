a1f862bee7e3899325e6ee70c23d090653d02b4a
Jcarrazco <48524432+Jcarrazco@users.noreply.github.com>
2019-03-27 17:51:37 -0600

    Add files via upload

---
+++ ServiciosReservacionDependientesTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservaciones.Entidades;
+using Reservaciones.Servicios;
+using NUnit.Framework;
+using Moq;
+using Reservaciones.Servicios.BaseDeDatos;
+
+namespace Reservaciones.UnitTests
+{
+    [TestFixture]
+    public class ServiciosReservacionDependientesTests
+    {
+        #region Test_BuscarHabitaciones
+        [Test]
+        public void BuscarHabitaciones_FechaEntradaMayorFechaSalida_Error()
+        {
+            //Arrange
+            var servicio = new ServiciosReservacionDependientes();
+            var usuario = new Usuario();
+            DateTime FechaIn = DateTime.Now;
+            DateTime FechaOut = DateTime.Now.AddDays(-1);//Fecha de salida menor a entrada
+            List<Habitacion> Habitaciones = new List<Habitacion>
+            { new Habitacion {Id=1, Ocupacion = EstadoOcupacion.Vacio },
+              new Habitacion {Id=2, Ocupacion = EstadoOcupacion.Vacio },};
+            List<Reservacion> Reservaciones = new List<Reservacion> {
+                new Reservacion{Id = 1, Usuario = usuario } };
+                //Mock servicios 
+            var ObjetoFalsoHabitaciones = new Mock<IRepositorioHabitaciones>();
+            ObjetoFalsoHabitaciones
+                .Setup(m => m.SeleccionarhabitacionesDisponibles(FechaIn, FechaOut))
+                .Returns(Habitaciones);
+            var ObjetoFalsoReservaciones = new Mock<IRepositorioReservaciones>();
+            ObjetoFalsoReservaciones
+                .Setup(m => m.BuscarReservacionesPorUsuario(usuario))
+                .Returns(Reservaciones);
+            //Act
+            void Metodo() => servicio.BuscarHabitaciones(FechaIn,FechaOut,usuario);
+            //Assert ArgumentException
+            Assert.Throws<ArgumentException>(Metodo);
+        }
+
+        [Test]
+        public void BuscarHabitaciones_HabitacionesNoReservadasLista()
+        {
+            //agregar habitaciones, que el usuario no haya reservado en esas fechas, a los resultados
+            //Arrange
+            
+            var usuario = new Usuario() { Id = 555};
+            var HabitaReserv = new Habitacion { Id = 11, Ocupacion = EstadoOcupacion.Ocupado };
+            var CostHabitaReserv = new CostoHabitacionPorDia(HabitaReserv, 100m, DateTime.Now);
+
+            DateTime FechaIn = DateTime.Now;
+            DateTime FechaOut = DateTime.Now.AddDays(4);//Fecha de salida menor a entrada
+
+            List<Habitacion> Habitaciones = new List<Habitacion>
+            { new Habitacion {Id=1, Ocupacion = EstadoOcupacion.Ocupado},
+              new Habitacion {Id=2, Ocupacion = EstadoOcupacion.Vacio },
+              new Habitacion {Id=3, Ocupacion = EstadoOcupacion.Vacio },
+              new Habitacion {Id=4, Ocupacion = EstadoOcupacion.Vacio }};
+
+            //Habitaciones.Add(new Habitacion { });
+
+            List<Reservacion> Reservaciones = new List<Reservacion> {
+                new Reservacion{Id = 11, Usuario = usuario,
+                    HabitacionesReservadas = new List<CostoHabitacionPorDia> {CostHabitaReserv } }
+                 };
+            //Mock servicios 
+            var ObjetoFalsoHabitaciones = new Mock<IRepositorioHabitaciones>();
+            ObjetoFalsoHabitaciones
+                .Setup(m => m.SeleccionarhabitacionesDisponibles(FechaIn, FechaOut))
+                .Returns(Habitaciones);
+            var ObjetoFalsoReservaciones = new Mock<IRepositorioReservaciones>();
+            ObjetoFalsoReservaciones
+                .Setup(m => m.BuscarReservacionesPorUsuario(usuario))
+                .Returns(Reservaciones);
+
+            var servicio = new ServiciosReservacionDependientes(ObjetoFalsoHabitaciones.Object,
+                                                                ObjetoFalsoReservaciones.Object);
+            //Act
+            List<CostoHabitacionPorDia> result = servicio.BuscarHabitaciones(FechaIn, FechaOut, usuario);
+            //Assert ArgumentException
+            Assert.IsFalse(result.Contains(CostHabitaReserv));
+        }
+        #endregion
+    }
+}
---
+++ ServiciosReservacionLocalesTests.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservaciones.Entidades;
+using Reservaciones.Servicios;
+using NUnit.Framework;
+
+namespace Reservaciones.UnitTests
+{
+    [TestFixture]
+    public class ServiciosReservacionLocalesTests
+    {
+        #region FiltrarBusquedaDeHabitaciones
+        [Test]
+        public void FiltrarBusquedaDeHabitaciones_NoEncontrado()
+        {
+            //Arrange
+            var servicio = new ServiciosReservacionLocales();
+            var hotel = new Hotel() {Categoria = Categoria.CincoEstrellas ,
+                Amenidades = new List<Amenidad> { Amenidad.AguaCaliente } };
+
+            var habitacionesDisp = new List<Habitacion> {
+                                    new Habitacion{Id=11, Tipo = TipoHbitacion.Doble, Hotel = hotel} };
+
+            var Tipos = new List<TipoHbitacion>();
+            var Amenidades = new List<Amenidad>();
+            var Categories = new List<Categoria>();
+
+            //Act
+            var resultado = servicio.FiltrarBusquedaDeHabitaciones(
+                habitacionesDisp, Tipos, Amenidades, Categories);
+            //Assert
+            Assert.AreEqual(new List<Habitacion>(), resultado );
+
+        }
+
+        [Test]
+        public void FiltrarBusquedaDeHabitaciones_TiposDiferentes()
+        {
+            //Arrange
+            var servicio = new ServiciosReservacionLocales();
+            var hotel = new Hotel()
+            {
+                Categoria = Categoria.CincoEstrellas,
+                Amenidades = new List<Amenidad> { Amenidad.AguaCaliente }
+            };
+            var habitac = new Habitacion { Id = 5, Tipo = TipoHbitacion.Doble, Hotel = hotel };
+            var habitacionesDisp = new List<Habitacion> {
+                                    habitac,
+                                    new Habitacion{Id=11, Tipo = TipoHbitacion.Doble, Hotel = hotel} };
+
+            var Tipos = new List<TipoHbitacion>() {TipoHbitacion.Doble };
+            var Amenidades = new List<Amenidad>();
+            var Categories = new List<Categoria>();
+
+            //Act
+            var resultado = servicio.FiltrarBusquedaDeHabitaciones(
+                habitacionesDisp, Tipos, Amenidades, Categories);
+            //Assert
+            Assert.IsTrue(resultado.Contains(habitac));
+        }
+
+        [Test]
+        public void FiltrarBusquedaDeHabitaciones_AmenidadesDiferentes()
+        {
+            //Arrange
+            var servicio = new ServiciosReservacionLocales();
+            var hotel = new Hotel()
+            {
+                Categoria = Categoria.CincoEstrellas,
+                Amenidades = new List<Amenidad> { Amenidad.AguaCaliente }
+            };
+            var habitac = new Habitacion { Id = 5, Tipo = TipoHbitacion.Doble, Hotel = hotel };
+            var habitacionesDisp = new List<Habitacion> {
+                                    habitac,
+                                    new Habitacion{Id=11, Tipo = TipoHbitacion.Doble, Hotel = hotel} };
+
+            var Tipos = new List<TipoHbitacion>();
+            var Amenidades = new List<Amenidad>() { Amenidad.AguaCaliente};
+            var Categories = new List<Categoria>();
+
+            //Act
+            var resultado = servicio.FiltrarBusquedaDeHabitaciones(
+                habitacionesDisp, Tipos, Amenidades, Categories);
+            //Assert
+            Assert.IsTrue(resultado.Contains(habitac));
+        }
+
+        [Test]
+        public void FiltrarBusquedaDeHabitaciones_CategoriasDiferentes()
+        {
+            //Arrange
+            var servicio = new ServiciosReservacionLocales();
+            var hotel = new Hotel()
+            {
+                Categoria = Categoria.CincoEstrellas,
+                Amenidades = new List<Amenidad> { Amenidad.AguaCaliente }
+            };
+            var habitac = new Habitacion { Id = 5, Tipo = TipoHbitacion.Doble, Hotel = hotel };
+            var habitacionesDisp = new List<Habitacion> {
+                                    habitac,
+                                    new Habitacion{Id=11, Tipo = TipoHbitacion.Doble, Hotel = hotel} };
+
+            var Tipos = new List<TipoHbitacion>();
+            var Amenidades = new List<Amenidad>();
+            var Categories = new List<Categoria>() { Categoria.CincoEstrellas};
+
+            //Act
+            var resultado = servicio.FiltrarBusquedaDeHabitaciones(
+                habitacionesDisp, Tipos, Amenidades, Categories);
+            //Assert
+            Assert.IsTrue(resultado.Contains(habitac));
+        }
+        #endregion
+
+        #region GenerarPrecioDeHabitacionPorFechaYUsuario
+        [Test]
+        public void GenerarPrecioDeHabitacionPorFechaYUsuario_DescuentoPremium()
+        {
+            //si recibimos un usuario y es un usuario premium, aplicar descuento de 10%
+            //Arrange
+            var usuario = new Usuario {Id=676 , EsUsuarioPremium = true };
+            var habitacion = new Habitacion {Id=4545, TarifaBase = 500m };
+            var dia = new DateTime(20000901);
+            var servicio = new ServiciosReservacionLocales();
+            //Act
+            var resultado = servicio.GenerarPrecioDeHabitacionPorFechaYUsuario(
+                habitacion, dia, usuario);
+            //Assert
+            Assert.AreEqual(450m, resultado.Precio);
+        }
+
+        [Test]
+        public void GenerarPrecioDeHabitacionPorFechaYUsuario_TarifaFriday()
+        {
+            ///si recibimos fecha, aplicar tarifas de fin de semana
+            //Arrange
+            var usuario = new Usuario { Id = 676, EsUsuarioPremium = false };
+            var habitacion = new Habitacion { Id = 4545, TarifaBase = 500m };
+            var dia = new DateTime(2019,03, 29);
+            var servicio = new ServiciosReservacionLocales();
+            //Act
+            var resultado = servicio.GenerarPrecioDeHabitacionPorFechaYUsuario(
+                habitacion, dia, usuario);
+            //Assert
+            Assert.AreEqual(525m, resultado.Precio);
+        }
+
+        [Test]
+        public void GenerarPrecioDeHabitacionPorFechaYUsuario_TarifaSatSunDay()
+        {
+            //si recibimos fecha, aplicar tarifas de fin de semana
+            //Arrange
+            var usuario = new Usuario { Id = 676, EsUsuarioPremium = false };
+            var habitacion = new Habitacion { Id = 4545, TarifaBase = 500m };
+            var dia = new DateTime(2019, 03, 30);
+            var servicio = new ServiciosReservacionLocales();
+            //Act
+            var resultado = servicio.GenerarPrecioDeHabitacionPorFechaYUsuario(
+                habitacion, dia, usuario);
+            //Assert
+            Assert.AreEqual(575m, resultado.Precio);
+        }
+
+        [Test]
+        public void GenerarPrecioDeHabitacionPorFechaYUsuario_TarifaSatSunDay_Premium()
+        {
+            //si recibimos un usuario y es un usuario premium, aplicar descuento de 10%
+            //si recibimos fecha, aplicar tarifas de fin de semana
+            //Arrange
+            var usuario = new Usuario { Id = 676, EsUsuarioPremium = true };
+            var habitacion = new Habitacion { Id = 4545, TarifaBase = 500m };
+            var dia = new DateTime(2019, 03, 30);
+            var servicio = new ServiciosReservacionLocales();
+            //Act
+            var resultado = servicio.GenerarPrecioDeHabitacionPorFechaYUsuario(
+                habitacion, dia, usuario);
+            //Assert
+            Assert.AreEqual(525m, resultado.Precio);
+        }
+        #endregion
+
+        #region GenerarPrecioDeHabitacionPorFechaYUsuario
+        [Test]
+        public void GenerarPrecioDeHabitacionPorFechaYUsuario_valida_precios()
+        {
+            // Regresa el precio de la ha bitacion por dia para cada dia del rango de fechas
+            var habitacion = new Habitacion() {Tipo = TipoHbitacion.Sencilla,
+                                            Ocupacion = EstadoOcupacion.Vacio,
+                                            TarifaBase = 500m};//usuario premium
+
+            var usuario = new Usuario { Id = 56, EsUsuarioPremium = true };
+            var ListaResult = new List<CostoHabitacionPorDia>();
+            var servicios = new ServiciosReservacionLocales();//servicio
+
+            var Finicio = new DateTime(2019,03,25);
+            var Ffin = new DateTime(2019,03,31);
+            //Act
+            ListaResult = servicios.GenerarPrecioDeHabitacionPorFechaYUsuario(
+                habitacion,Finicio,Ffin,usuario);
+            decimal total = ListaResult.Sum(item => item.Precio);
+
+            Assert.AreEqual(3325m, total);
+
+
+            //Assert
+            //foreach (CostoHabitacionPorDia CostoXd in ListaResult)
+            //{
+            //    Assert.AreEqual(servicios.GenerarPrecioDeHabitacionPorFechaYUsuario(habitacion, CostoXd.Fecha, usuario).Precio, CostoXd.Precio);
+            //}
+
+
+        }
+        #endregion
+    }
+}
